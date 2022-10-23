8fb30ea187e0a6bed7ad722d7f78cb59a8facd0e
OliverX <54567259+OliverXian@users.noreply.github.com>
2022-10-23 16:58:39 +0800

    Update avata

--- layouts/layout.js
+++ layouts/layout.js
@@ -42,7 +42,7 @@ const Layout = ({
                   alt={BLOG.author}
                   width={24}
                   height={24}
-                  src={`https://gravatar.com/avatar/${emailHash}`}
+                  src={`https://gravatar.com/avatar/c3e7add8f639fc120b76055b33519993`}
                   className="rounded-full"
                 />
                 <p className="ml-2 md:block">{BLOG.author}</p>
