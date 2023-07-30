a2a5038a4bc03edf6779741a18aca9c5a9fffbc0
OliverX <54567259+OliverXian@users.noreply.github.com>
2023-07-30 22:19:10 +0800

    Add GA

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Oliver',
   email: 'fdq0vzz1w@mozmail.com',
   link: 'https://blog.olioliver.com/',
-  description: 'Hello, glad to have you here!',
+  description: 'Hello, nice to have you here!',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -26,14 +26,14 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-70SHKJ40J2' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
